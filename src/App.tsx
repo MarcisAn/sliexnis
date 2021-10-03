c5c74fafac1502516513e666e00e28d2e0eb24ee
Mārcis <marcis-andersons@inbox.lv>
2021-10-03 19:27:38 +0300

    add title for production

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,8 @@
-import PrivateRoute from "./components/PrivateRoute";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import { AuthProvider } from "./contexts/AuthContext";
-import Landing from "./components/Landing";
-import UserContainer from "./components/UserContainer";
-
 function App() {
   return (
     <div>
-      <Router>
-        <AuthProvider>
-          <PrivateRoute exact path="/" component={UserContainer} />
-          <Route path="/landing" component={Landing} />
-        </AuthProvider>
-      </Router>
+      <h1>Sliexnis</h1>
+      <p>In the making...</p>
     </div>
   );
 }
