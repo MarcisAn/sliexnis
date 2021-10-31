20ef2340a28d04a01bdeffd7d3202d7967ef36de
Mārcis <marcis-andersons@inbox.lv>
2021-10-31 11:02:56 +0200

    update netlify function

--- nextjs-client/pages/api/hello.ts
+++ nextjs-client/pages/api/hello.ts
@@ -1,13 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-
+const { VARIABLE } = process.env;
 type Data = {
   name: string
+  variable: string
 }
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  if (VARIABLE) {
+    res.status(200).json({ name: 'John Doe', variable: VARIABLE })    
+  }
+  else {
+    res.status(404)
+  }
 }
