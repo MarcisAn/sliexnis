2036d3bf039e1a7f57d62eac55c52cee4fcb8868
Mārcis <marcis-andersons@inbox.lv>
2021-11-19 16:32:58 +0200

    add netlify redirects

--- nextjs-client/next.config.js
+++ nextjs-client/next.config.js
@@ -6,31 +6,31 @@ module.exports = {
     return [
       {
         source: "/src",
-        destination: "https://github.com/marcis-andersons/sliexnis",
+        destination: "https://github.com/marcis-andersons/sliexnis/",
         permanent: false,
         basePath: false,
       },
       {
         source: "/privacy-policy",
-        destination: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+        destination: "https://www.youtube.com/watch?v=dQw4w9WgXcQ/",
         permanent: false,
         basePath: false,
       },
       {
         source: "/terms-of-use",
-        destination: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+        destination: "https://www.youtube.com/watch?v=dQw4w9WgXcQ/",
         permanent: false,
         basePath: false,
       },
       {
         source: "/github",
-        destination: "https://github.com/marcis-andersons/sliexnis",
+        destination: "https://github.com/marcis-andersons/sliexnis/",
         permanent: false,
         basePath: false,
       },
       {
         source: "/twitter",
-        destination: "https://twitter.com/rickastley",
+        destination: "https://twitter.com/rickastley/",
         permanent: false,
         basePath: false,
       },
