f6b5050b6deac410556ad5352a176b514abfe41e
Mārcis <marcis-andersons@inbox.lv>
2021-11-20 15:00:36 +0200

    remove google analytics

--- nextjs-client/firebase.ts
+++ nextjs-client/firebase.ts
@@ -1,7 +1,6 @@
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
-import { getAnalytics } from "firebase/analytics";
 
 
 const firebaseConfig = {
@@ -13,7 +12,6 @@ const firebaseConfig = {
     appId: "1:186265093215:web:e30702c571cf7c640707ea"
   };
 const app = !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
-export const analytics = getAnalytics();
 export const auth = app.auth();
 export const firestore = app.firestore();
 export default app;
